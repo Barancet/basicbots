9ee98d6c6452fc84165013b732851646009f2a8a
Baran Cetin <barcet95@gmail.com>
2020-01-16 19:33:17 -0500

    Update OakCutter.java

--- BCWoodcutter/src/woodcutter/OakCutter.java
+++ BCWoodcutter/src/woodcutter/OakCutter.java
@@ -8,27 +8,22 @@ import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
 import org.dreambot.api.wrappers.interactive.GameObject;
 
-
 @ScriptManifest(category = Category.WOODCUTTING, name = "BCOakcutter" , author = "Baran", version = 1.0 )
 public class OakCutter extends AbstractScript {
-
     private int state;
     private Area oakArea;
     GameObject oakObject;
     private Tile t;
-
+    
     public OakCutter() {
         state = 0;
         oakArea = new Area(3103, 3246, 3100,3241);
         t = new Tile(3102, 3242);
-
     }
-
+    
     @Override
     public void onStart() {
         super.onStart();
-
-
     }
 
     @Override
@@ -41,23 +36,19 @@ public class OakCutter extends AbstractScript {
         return Calculations.random(2000,3400);
     }
 
-
     public void cut() {
         if(!getLocalPlayer().isAnimating()) {
             if(getInventory().isFull()){
                 state = 1;
             }
-
             oakObject = getGameObjects().getTopObjectOnTile(t);
             if (oakObject != null) {
                 oakObject.interact("Chop down");
             }
         }
-
     }
 
     public void bank() {
-
         if (!getBank().isOpen()) {
             if(getLocalPlayer().distance(getBank().getClosestBankLocation().getCenter()) > 5){
                 if(getWalking().walk(getBank().getClosestBankLocation().getCenter())){
@@ -70,7 +61,6 @@ public class OakCutter extends AbstractScript {
                     sleepUntil(() -> !getBank().isOpen(), Calculations.random(2000, 2900));
                     state = 0;
                 }
-
             }
         }
     }
